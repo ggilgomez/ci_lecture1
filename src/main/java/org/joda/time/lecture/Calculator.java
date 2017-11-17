a4f8b58302c072339f9c741ce7bfabff6906bf17
Francisco Gomes <gomesf@c15glyv.local>
2017-11-17 17:05:25 +0100

    Included new class to test coverage increase

---
+++ src/main/java/org/joda/time/lecture/Calculator.java
@@ -0,0 +1,17 @@
+package org.joda.time.lecture;
+
+public class Calculator {
+
+	public int sum(int x, int y) {
+		return x + y;
+	}
+
+	public int sub(int x, int y) {
+		return x - y;
+	}
+	
+	public int mult(int x, int y) {
+		return x * y;
+	}
+	
+}
